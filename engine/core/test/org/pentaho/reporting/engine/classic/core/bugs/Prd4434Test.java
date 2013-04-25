73b7e248e7dfc39f3b6c736db36277d823316419
Thomas Morgner <git@rabbit-stew-dio.com>
2013-04-25 17:56:14 +0100

    PRD-4434: Cell formatting lost on export to excel. Fix + test report.

---
+++ engine/core/test/org/pentaho/reporting/engine/classic/core/bugs/Prd4434Test.java
@@ -0,0 +1,67 @@
+/*
+ * This program is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License, version 2.1 as published by the Free Software
+ * Foundation.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * program; if not, you can obtain a copy at http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
+ * or from the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * Copyright (c) 2005-2011 Pentaho Corporation.  All rights reserved.
+ */
+
+package org.pentaho.reporting.engine.classic.core.bugs;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+
+import junit.framework.TestCase;
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.pentaho.reporting.engine.classic.core.ClassicEngineBoot;
+import org.pentaho.reporting.engine.classic.core.MasterReport;
+import org.pentaho.reporting.engine.classic.core.modules.output.table.xls.ExcelReportUtil;
+import org.pentaho.reporting.engine.classic.core.testsupport.gold.GoldTestBase;
+import org.pentaho.reporting.libraries.resourceloader.ResourceManager;
+
+public class Prd4434Test extends TestCase
+{
+  public Prd4434Test()
+  {
+  }
+
+  protected void setUp() throws Exception
+  {
+    ClassicEngineBoot.getInstance().start();
+  }
+
+  public void testExcelExport() throws Exception
+  {
+    final File file = GoldTestBase.locateGoldenSampleReport("Prd-3625.prpt");
+    ResourceManager mgr = new ResourceManager();
+    mgr.registerDefaults();
+    final MasterReport report = (MasterReport) mgr.createDirectly(file, MasterReport.class).getResource();
+
+    final ByteArrayOutputStream bout = new ByteArrayOutputStream();
+    ExcelReportUtil.createXLS(report, bout);
+
+    HSSFWorkbook wb = new HSSFWorkbook(new ByteArrayInputStream(bout.toByteArray()));
+    final HSSFSheet sheetAt = wb.getSheetAt(0);
+    final HSSFRow row = sheetAt.getRow(0);
+    final HSSFCell cell0 = row.getCell(0);
+    assertEquals(Cell.CELL_TYPE_NUMERIC, cell0.getCellType());
+    assertEquals("yyyy-MM-dd", cell0.getCellStyle().getDataFormatString());
+    final HSSFCell cell1 = row.getCell(1);
+    assertEquals(Cell.CELL_TYPE_NUMERIC, cell1.getCellType());
+    assertEquals("#,###.00;(#,###.00)", cell1.getCellStyle().getDataFormatString());
+  }
+}
