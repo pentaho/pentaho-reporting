c66abe0690e939d8c616e827d2d11a980b6651d1
Gretchen Moran <gmoran@pentaho.com>
2013-04-25 23:44:49 -0400

    PRD-3965 fix broken JUnit test (KettleDataFactoryTest.testMetadata())

--- engine/extensions-kettle/source/org/pentaho/reporting/engine/classic/extensions/datasources/kettle/KettleTransFromFileProducer.java
+++ engine/extensions-kettle/source/org/pentaho/reporting/engine/classic/extensions/datasources/kettle/KettleTransFromFileProducer.java
@@ -160,15 +160,6 @@ public class KettleTransFromFileProducer extends AbstractKettleTransformationPro
     {
       retval.add(resourceKey);
     }
-    try
-    {
-      TransMeta meta = loadTransformation(null, resourceManager, resourceKey);
-      retval.add(meta.hashCode());
-    }
-    catch (Exception e)
-    {
-      // if it fails, move along with original query hash ...
-    }
     retval.add(transformationFile);
     return retval;
   }
