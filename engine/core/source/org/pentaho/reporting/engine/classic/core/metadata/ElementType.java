9cb9ef966805d373f077ed8b44dde1334ba4fc88
Thomas Morgner <git@rabbit-stew-dio.com>
2013-06-05 15:16:58 +0100

    PRD-4379: Added API documentation and a missing helper method.

--- engine/core/source/org/pentaho/reporting/engine/classic/core/metadata/ElementType.java
+++ engine/core/source/org/pentaho/reporting/engine/classic/core/metadata/ElementType.java
@@ -32,11 +32,49 @@ import org.pentaho.reporting.engine.classic.core.function.ExpressionRuntime;
  */
 public interface ElementType extends DataSource
 {
+  /**
+   * Returns the element metadata for this element type.
+   *
+   * @return the element meta-data.
+   */
   public ElementMetaData getMetaData();
 
+  /**
+   * Compute a design-time value. This value will be displayed when editing the element in the
+   * Pentaho Report Designer. If there is not enough data available to produce a sensible output,
+   * return a mock-object so that the user can interact with the element in a sensible fashion.
+   * <p/>
+   * When the element is called, all style and attribute expressions have been resolved and can be
+   * accessed via the normal static style and attribute accessor methods. If the content of your
+   * element depends on style information, the fully computed style must be retrieved via the
+   * {@link org.pentaho.reporting.engine.classic.core.Element#getComputedStyle()} method. The
+   * local style-sheet for the element may not have all information.
+   *
+   * @param runtime the expression runtime holding the current report state.
+   * @param element the element.
+   * @return the computed value.
+   */
   public Object getDesignValue(ExpressionRuntime runtime, final ReportElement element);
 
+  /**
+   * Compute the design-time defaults that should be applied to an element after it was created by
+   * a design-tool. Only declare explicit values. If you assume defaults when an attribute or style
+   * is undefined, then there is no need to set it here.
+   * <p/>
+   * Any value set here will be treated as user-defined value afterwards and will be explicitly
+   * stored in the XML files.
+   *
+   * @param element the element that should be configured.
+   * @param locale the locale.
+   */
   public void configureDesignTimeDefaults(ReportElement element, Locale locale);
 
+  /**
+   * Creates the element implementation. Return a new {@link org.pentaho.reporting.engine.classic.core.Element}
+   * for data-items, {@link org.pentaho.reporting.engine.classic.core.Band} for compound data items or
+   * the appropriate Element implementation for complex element types.
+   *
+   * @return the new element instance.
+   */
   public ReportElement create();
 }
