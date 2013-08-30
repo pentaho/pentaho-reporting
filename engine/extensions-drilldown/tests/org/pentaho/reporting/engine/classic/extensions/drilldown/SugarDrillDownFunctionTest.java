9545eaacfc12d535dd79ad3a8d72e5c7b5e32abd
mkambol <mkambol@gmail.com>
2013-08-30 12:02:25 -0400

    [PRD-4697] Added drill profiles for xanalyzer, prpti, and xaction types.  This will make those file types visible when browsing for a link target, as well as add custom handling of the URLs for each type.

--- engine/extensions-drilldown/tests/org/pentaho/reporting/engine/classic/extensions/drilldown/SugarDrillDownFunctionTest.java
+++ engine/extensions-drilldown/tests/org/pentaho/reporting/engine/classic/extensions/drilldown/SugarDrillDownFunctionTest.java
@@ -42,7 +42,20 @@ public class SugarDrillDownFunctionTest extends FormulaTestBase
             "ssh://domain.example/api/repos/:public:steel-wheels:test.prpt/content?test=value&mtest=v1&mtest=v2&mtest=v3"},
         {"DRILLDOWN(\"remote-sugar-no-parameter\"; \"ssh://domain.example/\"; {\"::pentaho-path\" ; \"/public/steel-wheels/test.prpt\" | \"test\" ; \"value\" | \"mtest\" ; {\"v1\"; \"v2\"; \"v3\" }})",
             "ssh://domain.example/api/repos/:public:steel-wheels:test.prpt/content?test=value&mtest=v1&mtest=v2&mtest=v3"},
+        {"DRILLDOWN(\"remote-sugar-prpti\"; \"http://domain.example/\"; {\"::pentaho-path\" ; \"/public/steel-wheels/test.prpt\" | \"test\" ; \"value\" | \"mtest\" ; {\"v1\"; \"v2\"; \"v3\" }})",
+            "http://domain.example/api/repos/:public:steel-wheels:test.prpt/prpti.view?test=value&mtest=v1&mtest=v2&mtest=v3"},
+        {"DRILLDOWN(\"local-sugar-prpti\"; 0; {\"::pentaho-path\" ; \"/public/steel-wheels/test.prpt\" | \"test\" ; \"value\" | \"mtest\" ; {\"v1\"; \"v2\"; \"v3\" }})",
+            "http://localhost:8080/pentaho/api/repos/:public:steel-wheels:test.prpt/prpti.view?test=value&mtest=v1&mtest=v2&mtest=v3"},
+        {"DRILLDOWN(\"local-sugar-analyzer\"; 0; {\"::pentaho-path\" ; \"/public/steel-wheels/test.prpt\" | \"test\" ; \"value\" | \"mtest\" ; {\"v1\"; \"v2\"; \"v3\" }})",
+            "http://localhost:8080/pentaho/api/repos/:public:steel-wheels:test.prpt/viewer?test=value&mtest=v1&mtest=v2&mtest=v3"},
+        {"DRILLDOWN(\"local-sugar-xaction\"; 0; {\"::pentaho-path\" ; \"/public/steel-wheels/test.prpt\" | \"test\" ; \"value\" | \"mtest\" ; {\"v1\"; \"v2\"; \"v3\" }})",
+            "http://localhost:8080/pentaho/api/repos/:public:steel-wheels:test.prpt/generatedContent?test=value&mtest=v1&mtest=v2&mtest=v3"},
+        {"DRILLDOWN(\"remote-sugar-analyzer\"; \"http://domain.example/\"; {\"::pentaho-path\" ; \"/public/steel-wheels/test.prpt\" | \"test\" ; \"value\" | \"mtest\" ; {\"v1\"; \"v2\"; \"v3\" }})",
+            "http://domain.example/api/repos/:public:steel-wheels:test.prpt/viewer?test=value&mtest=v1&mtest=v2&mtest=v3"},
+        {"DRILLDOWN(\"remote-sugar-xaction\"; \"http://domain.example/\"; {\"::pentaho-path\" ; \"/public/steel-wheels/test.prpt\" | \"test\" ; \"value\" | \"mtest\" ; {\"v1\"; \"v2\"; \"v3\" }})",
+            "http://domain.example/api/repos/:public:steel-wheels:test.prpt/generatedContent?test=value&mtest=v1&mtest=v2&mtest=v3"},
     };
+
   }
 
   public void testDefault() throws Exception
