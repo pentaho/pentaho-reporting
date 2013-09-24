de0d462222c970b6a41029ec78cd9bc33e4b801c
Thomas Morgner <git@rabbit-stew-dio.com>
2013-09-24 18:25:32 +0100

    PRD-4738 - Table exports with Assume-overflow-x set to true caused export errors (cherry picked from commit fd7fb7c)

---
+++ engine/core/test/org/pentaho/reporting/engine/classic/core/bugs/Prd4738Test.java
@@ -0,0 +1,49 @@
+package org.pentaho.reporting.engine.classic.core.bugs;
+
+import junit.framework.TestCase;
+import org.pentaho.reporting.engine.classic.core.ClassicEngineBoot;
+import org.pentaho.reporting.engine.classic.core.Element;
+import org.pentaho.reporting.engine.classic.core.MasterReport;
+import org.pentaho.reporting.engine.classic.core.layout.ModelPrinter;
+import org.pentaho.reporting.engine.classic.core.layout.model.LayoutNodeTypes;
+import org.pentaho.reporting.engine.classic.core.layout.model.LogicalPageBox;
+import org.pentaho.reporting.engine.classic.core.layout.model.RenderNode;
+import org.pentaho.reporting.engine.classic.core.layout.table.TableTestUtil;
+import org.pentaho.reporting.engine.classic.core.modules.output.table.xls.ExcelReportUtil;
+import org.pentaho.reporting.engine.classic.core.testsupport.DebugReportRunner;
+import org.pentaho.reporting.engine.classic.core.testsupport.selector.MatchFactory;
+
+public class Prd4738Test extends TestCase
+{
+  public Prd4738Test()
+  {
+  }
+
+  protected void setUp() throws Exception
+  {
+    ClassicEngineBoot.getInstance().start();
+  }
+
+  public void testBug() throws Exception
+  {
+    final MasterReport report = new MasterReport();
+    report.getReportHeader().setLayout("row");
+    report.getReportHeader().addElement(TableTestUtil.createDataItem("Test1"));
+    report.getReportHeader().addElement(TableTestUtil.createDataItem("Test2"));
+    report.getReportConfiguration().setConfigProperty
+        ("org.pentaho.reporting.engine.classic.core.modules.output.table.xml.AssumeOverflowX", "true");
+    report.getReportConfiguration().setConfigProperty
+        ("org.pentaho.reporting.engine.classic.core.modules.output.table.xls.AssumeOverflowX", "true");
+    final LogicalPageBox box = DebugReportRunner.layoutTablePage(report, 0);
+    ModelPrinter.INSTANCE.print(box);
+
+    final RenderNode[] elementsByNodeType = MatchFactory.findElementsByNodeType(box, LayoutNodeTypes.TYPE_BOX_PARAGRAPH);
+    for (int i = 0; i < elementsByNodeType.length; i++)
+    {
+      RenderNode renderNode = elementsByNodeType[i];
+      assertEquals(10000000, renderNode.getWidth());
+    }
+
+    // ExcelReportUtil.createXLS(report, "test-output/Prd4738.xls");
+  }
+}
