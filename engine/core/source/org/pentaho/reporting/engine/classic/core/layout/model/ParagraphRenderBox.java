1a6bd928f361b0a7f62aafd1c483d9f07be4ad31
Thomas Morgner <tmorgner@pentaho.com>
2014-04-09 14:14:58 +0100

    [PRD-4897] - MinChunkWidth is useless if we do not break on word-boundaries.

--- engine/core/source/org/pentaho/reporting/engine/classic/core/layout/model/ParagraphRenderBox.java
+++ engine/core/source/org/pentaho/reporting/engine/classic/core/layout/model/ParagraphRenderBox.java
@@ -28,7 +28,6 @@ import org.pentaho.reporting.engine.classic.core.style.ElementStyleKeys;
 import org.pentaho.reporting.engine.classic.core.style.StyleSheet;
 import org.pentaho.reporting.engine.classic.core.style.TextStyleKeys;
 import org.pentaho.reporting.engine.classic.core.util.InstanceID;
-import org.pentaho.reporting.libraries.base.util.DebugLog;
 
 /**
  * Creation-Date: 03.04.2007, 13:38:00
@@ -350,10 +349,6 @@ public final class ParagraphRenderBox extends BlockRenderBox
 
   public void setCachedWidth(final long cachedWidth)
   {
-    if ("canvas-ci".equals(getName()))
-    {
-      DebugLog.logHere();
-    }
     super.setCachedWidth(cachedWidth);
   }
 }
