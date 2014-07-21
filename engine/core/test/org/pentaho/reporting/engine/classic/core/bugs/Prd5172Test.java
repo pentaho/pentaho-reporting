8454a0aa72822c522191fbc034bb21526c4e9b74
Thomas Morgner <tmorgner@pentaho.com>
2014-07-21 17:12:46 +0100

    [SP-1377] - Regression of PRD-5172 - PRPT created in PRD 3.9.3 does not open in 5.0.5, 5.0.6, 5.0.7 and 5.1 (5.1)
    cherry-pick from master of https://github.com/pentaho/pentaho-reporting/commit/93da5108965f05cfa7e95811730dc9244b6335b6

---
+++ engine/core/test/org/pentaho/reporting/engine/classic/core/bugs/Prd5172Test.java
@@ -0,0 +1,66 @@
+/*
+ * This program is free software; you can redistribute it and/or modify it under the
+ *  terms of the GNU Lesser General Public License, version 2.1 as published by the Free Software
+ *  Foundation.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License along with this
+ *  program; if not, you can obtain a copy at http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
+ *  or from the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ *  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *  See the GNU Lesser General Public License for more details.
+ *
+ *  Copyright (c) 2006 - 2009 Pentaho Corporation..  All rights reserved.
+ */
+
+package org.pentaho.reporting.engine.classic.core.bugs;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.pentaho.reporting.engine.classic.core.ClassicEngineBoot;
+import org.pentaho.reporting.engine.classic.core.MasterReport;
+import org.pentaho.reporting.engine.classic.core.SubReport;
+import org.pentaho.reporting.engine.classic.core.layout.model.LogicalPageBox;
+import org.pentaho.reporting.engine.classic.core.style.BandStyleKeys;
+import org.pentaho.reporting.engine.classic.core.testsupport.DebugReportRunner;
+
+public class Prd5172Test
+{
+  @Before
+  public void setUp() throws Exception
+  {
+    ClassicEngineBoot.getInstance().start();
+  }
+
+  @Test
+  public void testBandedSubReportIsInline() throws Exception
+  {
+    SubReport subReport = new SubReport();
+    subReport.getStyle().setStyleProperty(BandStyleKeys.LAYOUT, BandStyleKeys.LAYOUT_INLINE);
+
+    MasterReport report = new MasterReport();
+    report.getReportHeader().addSubReport(subReport);
+    LogicalPageBox logicalPageBox = DebugReportRunner.layoutPage(report, 0);
+  }
+
+  @Test
+  public void testInlineSubReportIsInline() throws Exception
+  {
+    SubReport subReport = new SubReport();
+    subReport.getStyle().setStyleProperty(BandStyleKeys.LAYOUT, BandStyleKeys.LAYOUT_INLINE);
+
+    MasterReport report = new MasterReport();
+    report.getReportHeader().addElement(subReport);
+    LogicalPageBox logicalPageBox = DebugReportRunner.layoutPage(report, 0);
+  }
+
+  @Test
+  public void testMasterReportInline() throws Exception
+  {
+    MasterReport report = new MasterReport();
+    report.getStyle().setStyleProperty(BandStyleKeys.LAYOUT, BandStyleKeys.LAYOUT_INLINE);
+    LogicalPageBox logicalPageBox = DebugReportRunner.layoutPage(report, 0);
+  }
+}
