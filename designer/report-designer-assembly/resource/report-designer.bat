92c020ccf4df7d9bc347caa2e525a5dff8018c74
Andrey Khayrutdinov <andrey_khayrutdinov@epam.com>
2015-03-30 15:13:04 +0400

    [PRD-5332] - wrong java version taken in Windows 7 with multiple java versions installed - changed the bat so, that it will respect _PENTAHO_JAVA_HOME variable; also updated the comments that relate to JDK's version

--- designer/report-designer-assembly/resource/report-designer.bat
+++ designer/report-designer-assembly/resource/report-designer.bat
@@ -1,10 +1,15 @@
 @echo off
 @REM
-@REM WARNING: Pentaho Report Designer needs JDK 1.6 or newer to run.
+@REM WARNING: Pentaho Report Designer needs JDK 1.7 or newer to run.
 @REM
 setlocal
 cd /D %~dp0
-set PENTAHO_JAVA=javaw
+set PENTAHO_JAVA=javaw.exe
+
+if "%_PENTAHO_JAVA_HOME%" == "" goto callSetEnv
+set PENTAHO_JAVA_HOME=%_PENTAHO_JAVA_HOME%
+
+:callSetEnv
 call "%~dp0set-pentaho-env.bat"
 
 start "Pentaho Report Designer" "%_PENTAHO_JAVA%" -XX:MaxPermSize=256m -Xmx512M -jar "%~dp0launcher.jar" %*
