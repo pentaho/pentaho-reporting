0889ed7f3496ad75f5395ffbce24083cf257abae
AndreyBurikhin <Andrey_Burikhin@epam.com>
2015-10-29 15:29:31 +0400

    [BACKLOG-5408] - tests for pentaho-reporting-engine-datasource-editor-jdbc

---
+++ designer/datasource-editor-jdbc/test-src/org/pentaho/reporting/ui/datasources/jdbc/SimpleJdbcDataSourcePluginTest.java
@@ -0,0 +1,69 @@
+/*
+ * This program is free software; you can redistribute it and/or modify it under the
+ *  terms of the GNU Lesser General Public License, version 2.1 as published by the Free Software
+ *  Foundation.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License along with this
+ *  program; if not, you can obtain a copy at http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
+ *  or from the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ *  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *  See the GNU Lesser General Public License for more details.
+ *
+ *  Copyright (c) 2006 - 2015 Pentaho Corporation..  All rights reserved.
+ */
+package org.pentaho.reporting.ui.datasources.jdbc;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.awt.Window;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.pentaho.reporting.engine.classic.core.DataFactory;
+import org.pentaho.reporting.engine.classic.core.designtime.DataFactoryChangeRecorder;
+import org.pentaho.reporting.engine.classic.core.designtime.DesignTimeContext;
+import org.pentaho.reporting.engine.classic.core.modules.misc.datafactory.sql.SimpleSQLReportDataFactory;
+import org.pentaho.reporting.ui.datasources.jdbc.ui.SimpleJdbcDataSourceDialog;
+
+public class SimpleJdbcDataSourcePluginTest {
+
+  private static final String PROPERTY_NAME = "java.awt.headless";
+
+  private static String defaultValue;
+
+  @BeforeClass
+  public static void beforeClass() {
+    defaultValue = System.getProperty( PROPERTY_NAME, Boolean.toString( true ) );
+    System.setProperty( PROPERTY_NAME, Boolean.toString( true ) );
+  }
+
+  @AfterClass
+  public static void afterClass() {
+    System.setProperty( PROPERTY_NAME, defaultValue );
+  }
+
+  @Test
+  public void performEdit() {
+    SimpleJdbcDataSourcePlugin simpleJdbcDataSourcePlugin = spy( new SimpleJdbcDataSourcePlugin() );
+    SimpleJdbcDataSourceDialog editor = mock( SimpleJdbcDataSourceDialog.class );
+    doReturn( editor ).when( simpleJdbcDataSourcePlugin ).createEditor( any( DesignTimeContext.class ) );
+    DesignTimeContext designTimeContext = mock( DesignTimeContext.class );
+    when( designTimeContext.getParentWindow() ).thenReturn( mock( Window.class ) );
+    DataFactory input = mock( SimpleSQLReportDataFactory.class );
+    DataFactoryChangeRecorder changeRecorder = mock( DataFactoryChangeRecorder.class );
+
+    simpleJdbcDataSourcePlugin.performEdit( designTimeContext, input, "TEST_QUERY_NAME", changeRecorder );
+    verify( editor, times( 1 ) ).performConfiguration( any( SimpleSQLReportDataFactory.class ) );
+  }
+
+}
---
+++ designer/datasource-editor-jdbc/test-src/org/pentaho/reporting/ui/datasources/jdbc/connection/JndiConnectionDefinitionTest.java
@@ -0,0 +1,54 @@
+/*
+ * This program is free software; you can redistribute it and/or modify it under the
+ *  terms of the GNU Lesser General Public License, version 2.1 as published by the Free Software
+ *  Foundation.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License along with this
+ *  program; if not, you can obtain a copy at http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
+ *  or from the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ *  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *  See the GNU Lesser General Public License for more details.
+ *
+ *  Copyright (c) 2006 - 2015 Pentaho Corporation..  All rights reserved.
+ */
+package org.pentaho.reporting.ui.datasources.jdbc.connection;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class JndiConnectionDefinitionTest {
+
+  private JndiConnectionDefinition connectionDefinition;
+
+  @Before
+  public void before() {
+    connectionDefinition =
+        new JndiConnectionDefinition( "TEST_NAME", "TEST_JNDI_NAME", "TEST_DATABASE_TYPE", "TEST_USER", "TEST_PASSWORD" );
+  }
+
+  @Test( expected = IllegalArgumentException.class )
+  public void exceptionOnEmptyJndiName() {
+    new JndiConnectionDefinition( "TEST_NAME", null, "TEST_DATABASE_TYPE", "TEST_USER", "TEST_PASSWORD" );
+  }
+
+  @Test
+  public void getUsername() {
+    assertEquals( "TEST_USER", connectionDefinition.getUsername() );
+  }
+
+  @Test
+  public void getPassword() {
+    assertEquals( "TEST_PASSWORD", connectionDefinition.getPassword() );
+  }
+
+  @Test
+  public void getDatabaseType() {
+    assertEquals( "TEST_DATABASE_TYPE", connectionDefinition.getDatabaseType() );
+  }
+
+}
