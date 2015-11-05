01c76df6a6dbb728ecc2a84a9b79fdf205c8fb59
Anna Popova1 <Anna_Popova1@epam.com>
2015-11-05 12:23:13 +0400

    [BACKLOG-5384] Removed unused module server

--- engine/server/test-src/org/pentaho/reporting/engine/classic/webapp/servlet/DummyTest.java
+++
@@ -1,9 +0,0 @@
-package org.pentaho.reporting.engine.classic.webapp.servlet;
-
-import junit.framework.TestCase;
-
-public class DummyTest extends TestCase {
-  public void testNothing() {
-
-  }
-}
