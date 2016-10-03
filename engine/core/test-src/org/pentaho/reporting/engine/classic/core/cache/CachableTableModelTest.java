0eec647cd68a77c0d2a0a481534e95312d2d0e6d
Denis Protopopov <denis_protopopov@epam.com>
2016-10-03 12:24:04 +0400

    [SP-2955][PRD-5757]-Results sets with booleans are not cached
     - added check for Booleans
     - added unit test
     - fixed unit test

---
+++ engine/core/test-src/org/pentaho/reporting/engine/classic/core/cache/CachableTableModelTest.java
@@ -0,0 +1,45 @@
+/*!
+ * This program is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License, version 2.1 as published by the Free Software
+ * Foundation.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * program; if not, you can obtain a copy at http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
+ * or from the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * Copyright (c) 2002-2016 Pentaho Corporation..  All rights reserved.
+ */
+
+package org.pentaho.reporting.engine.classic.core.cache;
+
+
+import org.junit.Test;
+import org.pentaho.reporting.engine.classic.core.util.TypedTableModel;
+import javax.swing.table.TableModel;
+import org.junit.Assert;
+import java.util.Date;
+
+public class CachableTableModelTest {
+  @Test
+  public void testIsSafeToCache() {
+    TypedTableModel model = new TypedTableModel( );
+    model.addColumn( "id", java.lang.String.class );
+    model.addColumn( "id", java.lang.Integer.class );
+    model.addColumn( "bb", java.lang.Boolean.class );
+    model.addColumn( "bb", java.util.Date.class );
+    model.addColumn( "id", java.lang.Integer.TYPE );
+    model.addRow( "R0", "C0", "1" );
+    model.addRow( "R0", "C1", 1 );
+    model.addRow( "R2", "C2", true );
+    model.addRow( "R2", "C3", new Date() );
+    model.addRow( "R0", "C1", 1 );
+
+    TableModel tableModel = new CachableTableModel( model );
+    Assert.assertTrue( CachableTableModel.isSafeToCache( tableModel ) );
+  }
+}
