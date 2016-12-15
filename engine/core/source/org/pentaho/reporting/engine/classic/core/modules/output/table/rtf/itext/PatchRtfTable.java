298d1c7c1328f4f926661ce6e719536c37cfc9cd
stanislau-strelchanka <stanislau_strelchanka@epam.com>
2016-12-15 12:16:03 +0300

    [PRD-5716] Running a report that returns multiple RTF worksheets (Flow Mode) runs into performance issues

--- engine/core/source/org/pentaho/reporting/engine/classic/core/modules/output/table/rtf/itext/PatchRtfTable.java
+++ engine/core/source/org/pentaho/reporting/engine/classic/core/modules/output/table/rtf/itext/PatchRtfTable.java
@@ -12,7 +12,7 @@
  *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  *  See the GNU Lesser General Public License for more details.
  *
- *  Copyright (c) 2006 - 2013 Pentaho Corporation..  All rights reserved.
+ *  Copyright (c) 2006 - 2016 Pentaho Corporation..  All rights reserved.
  */
 
 package org.pentaho.reporting.engine.classic.core.modules.output.table.rtf.itext;
@@ -138,6 +138,8 @@ public class PatchRtfTable extends RtfElement {
     Iterator rowIterator = table.iterator();
     while ( rowIterator.hasNext() ) {
       this.rows.add( new PatchRtfRow( this.document, this, (Row) rowIterator.next(), i ) );
+      // avoid out of memory exception
+      rowIterator.remove();
       i++;
     }
     for ( i = 0; i < this.rows.size(); i++ ) {
---
+++ engine/core/test-src/org/pentaho/reporting/engine/classic/core/modules/output/table/rtf/itext/PatchRtfTableTest.java
@@ -0,0 +1,51 @@
+/*
+ * This program is free software; you can redistribute it and/or modify it under the
+ *  terms of the GNU Lesser General Public License, version 2.1 as published by the Free Software
+ *  Foundation.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License along with this
+ *  program; if not, you can obtain a copy at http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
+ *  or from the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ *  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *  See the GNU Lesser General Public License for more details.
+ *
+ *  Copyright (c) 2006 - 2016 Pentaho Corporation..  All rights reserved.
+ */
+
+package org.pentaho.reporting.engine.classic.core.modules.output.table.rtf.itext;
+
+import com.lowagie.text.BadElementException;
+import com.lowagie.text.Row;
+import com.lowagie.text.Table;
+import com.lowagie.text.rtf.document.RtfDocument;
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class PatchRtfTableTest {
+
+  @Test
+  public void testImportTableRemovesUnusedRow() throws BadElementException {
+    Table table = Mockito.mock( Table.class );
+    Row row = Mockito.mock( Row.class );
+    RtfDocument rtfDocument = new RtfDocument();
+    List<Row> rows = new ArrayList<>( 5 );
+    for ( int i = 0; i < 5; i++ ) {
+      rows.add( row );
+    }
+    Iterator<Row> iterator = rows.iterator();
+
+    Mockito.when( table.iterator() ).thenReturn( iterator );
+
+    new PatchRtfTable( rtfDocument, table );
+    Assert.assertFalse( iterator.hasNext() );
+    Assert.assertEquals( 0, rows.size() );
+  }
+}
