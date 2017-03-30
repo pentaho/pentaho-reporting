276e6122bdd3f1119acc8d6ae92964d6871b4fb3
Egor Zhuk <Yahor_Zhuk@epam.com>
2017-03-30 10:44:23 +0300

    [PRD-3638]-Check the platform credentials faster

--- designer/report-designer-extension-pentaho/src/main/java/org/pentaho/reporting/designer/extensions/pentaho/repository/actions/ValidateLoginTask.java
+++ designer/report-designer-extension-pentaho/src/main/java/org/pentaho/reporting/designer/extensions/pentaho/repository/actions/ValidateLoginTask.java
@@ -12,26 +12,45 @@
 * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 * See the GNU Lesser General Public License for more details.
 *
-* Copyright (c) 2002-2013 Pentaho Corporation..  All rights reserved.
+* Copyright (c) 2002-2017 Pentaho Corporation..  All rights reserved.
 */
 
 package org.pentaho.reporting.designer.extensions.pentaho.repository.actions;
 
+import com.google.common.annotations.VisibleForTesting;
+import org.apache.commons.httpclient.Credentials;
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpMethod;
+import org.apache.commons.httpclient.HttpState;
+import org.apache.commons.httpclient.HttpStatus;
+import org.apache.commons.httpclient.UsernamePasswordCredentials;
+import org.apache.commons.httpclient.auth.AuthScope;
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.commons.vfs2.FileObject;
 import org.apache.commons.vfs2.FileSystem;
 import org.apache.commons.vfs2.FileSystemException;
 import org.apache.commons.vfs2.FileType;
 import org.apache.commons.vfs2.VFS;
+import org.pentaho.reporting.designer.core.ReportDesignerBoot;
 import org.pentaho.reporting.designer.core.auth.AuthenticationData;
 import org.pentaho.reporting.designer.extensions.pentaho.repository.util.PublishUtil;
 import org.pentaho.reporting.libraries.pensol.vfs.WebSolutionFileSystem;
 
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 public class ValidateLoginTask implements Runnable {
+  private static final String AUTHORIZATION = "Authorization";
+  private static final String TREE_API_URL_PART = "/api/repo/files/%3A/tree?depth=1";
   private AuthenticationData loginData;
   private Exception exception;
   private boolean loginComplete;
+
+  private static final Log logger = LogFactory.getLog( ValidateLoginTask.class );
 
   public ValidateLoginTask( final LoginTask loginTask ) {
     this.loginData = loginTask.getLoginData();
@@ -48,7 +67,11 @@ public class ValidateLoginTask implements Runnable {
   public void run() {
     loginComplete = false;
     try {
-      loginComplete = validateLoginData();
+      if ( useOldVfsValidation() ) {
+        loginComplete = validateLoginData();
+      } else {
+        loginComplete = validateLoginDataFast();
+      }
     } catch ( FileSystemException exception ) {
       this.loginComplete = false;
       this.exception = exception;
@@ -63,6 +86,13 @@ public class ValidateLoginTask implements Runnable {
     return loginComplete;
   }
 
+
+  /**
+   * An old implementation, which may take unreasonable amount of time in case of a huge filesystem Disabled by default.
+   * Could be switched on by setting a org.pentaho.reporting.designer.extensions.pentaho.repository.VfsValidation to
+   * true
+   */
+  @Deprecated
   public boolean validateLoginData() throws FileSystemException {
     if ( loginData == null ) {
       return true;
@@ -91,4 +121,63 @@ public class ValidateLoginTask implements Runnable {
     }
     return true;
   }
+
+
+  /**
+   * A faster way to check the provided credentials by calling a servers tree API with a basic authentication.
+   *
+   * @return
+   */
+  public boolean validateLoginDataFast() {
+
+    //Backward compatibility
+    if ( loginData == null ) {
+      return true;
+    }
+
+    try {
+
+      final HttpClient httpClient = getHttpClient();
+      //Remove trailing slashes and spaces
+      final URI uri = new URI( StringUtils.stripEnd( loginData.getUrl(), "/ " ) + TREE_API_URL_PART );
+      final Credentials credentials =
+        new UsernamePasswordCredentials( loginData.getUsername(), loginData.getPassword() );
+      final HttpState state = new HttpState();
+      state.setCredentials( new AuthScope( uri.getHost(), uri.getPort(), AuthScope.ANY_REALM ), credentials );
+      httpClient.setState( state );
+
+      final HttpMethod get = new GetMethod( uri.toString() );
+
+      final int code = httpClient.executeMethod( get );
+
+      if ( code == HttpStatus.SC_OK ) {
+        return true;
+      }
+
+      logger.info( "Can't log in with the provided credentials. Status code: " + code );
+
+    } catch ( final IOException | URISyntaxException | IllegalArgumentException e ) {
+      logger.info( "Can't connect to the server", e );
+    }
+    return false;
+  }
+
+  @VisibleForTesting
+  HttpClient getHttpClient() {
+    return new HttpClient();
+  }
+
+  @VisibleForTesting
+  boolean useOldVfsValidation() {
+    boolean result = false;
+    try {
+      final String useOldVfsValidation = ReportDesignerBoot.getInstance().getGlobalConfig().getConfigProperty(
+        "org.pentaho.reporting.designer.extensions.pentaho.repository.VfsValidation" );
+      result = "true".equals( useOldVfsValidation );
+    } catch ( final Exception e ) {
+      logger.info( "Cant't read a configuration: ", e );
+    }
+    return result;
+  }
+
 }
