16d6986f94ade60697df2ecd20a312794b6c3c43
PashkovichAndrei <andrei_pashkovich@epam.com>
2017-06-01 20:54:40 +0300

    [BISERVER-12968]-Self Links defined in a PRPT chart do not working on the server

--- engine/core/src/main/java/org/pentaho/reporting/engine/classic/core/function/formula/EnvFunction.java
+++ engine/core/src/main/java/org/pentaho/reporting/engine/classic/core/function/formula/EnvFunction.java
@@ -12,13 +12,16 @@
  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU Lesser General Public License for more details.
  *
- * Copyright (c) 2002-2013 Pentaho Corporation..  All rights reserved.
+ * Copyright (c) 2002-2017 Pentaho Corporation..  All rights reserved.
  */
 
 package org.pentaho.reporting.engine.classic.core.function.formula;
 
 import org.pentaho.reporting.engine.classic.core.ReportEnvironment;
+import org.pentaho.reporting.engine.classic.core.function.ExpressionRuntime;
+import org.pentaho.reporting.engine.classic.core.function.ProcessingContext;
 import org.pentaho.reporting.engine.classic.core.function.ReportFormulaContext;
+import org.pentaho.reporting.libraries.base.util.URLEncoder;
 import org.pentaho.reporting.libraries.formula.EvaluationException;
 import org.pentaho.reporting.libraries.formula.FormulaContext;
 import org.pentaho.reporting.libraries.formula.LibFormulaErrorValue;
@@ -26,8 +29,12 @@ import org.pentaho.reporting.libraries.formula.function.Function;
 import org.pentaho.reporting.libraries.formula.function.ParameterCallback;
 import org.pentaho.reporting.libraries.formula.lvalues.TypeValuePair;
 import org.pentaho.reporting.libraries.formula.typing.coretypes.AnyType;
+import org.pentaho.reporting.libraries.resourceloader.ResourceKey;
 
 public class EnvFunction implements Function {
+
+  private static final Object NULL_OBJECT = new Object();
+
   public EnvFunction() {
   }
 
@@ -47,6 +54,44 @@ public class EnvFunction implements Function {
 
     final ReportFormulaContext rfc = (ReportFormulaContext) context;
     final ReportEnvironment reportEnvironment = rfc.getProcessingContext().getEnvironment();
-    return new TypeValuePair( AnyType.TYPE, reportEnvironment.getEnvironmentProperty( fieldName ) );
+    Object property = reportEnvironment.getEnvironmentProperty( fieldName );
+
+    if ( "selfURL".equals( fieldName ) && property != null && !property.toString().isEmpty() ) {
+      final ReportFormulaContext tmp = (ReportFormulaContext) rfc.getBackend();
+      final ReportFormulaContext backend = tmp != null ? (ReportFormulaContext) tmp.getBackend() : null;
+      ExpressionRuntime runtime = backend != null ? backend.getRuntime() : null;
+      ProcessingContext processingContext = runtime != null ? runtime.getProcessingContext() : null;
+      ResourceKey contentBase = processingContext != null ? processingContext.getContentBase() : null;
+      ResourceKey parent = contentBase != null ? contentBase.getParent() : null;
+      Object identifier = parent != null ? parent.getIdentifier() : null;
+
+      String prop = convertToString( property );
+      String id = convertToString( identifier );
+
+      if ( prop != null && id != null && !prop.isEmpty() && !id.isEmpty() ) {
+        id = URLEncoder.encodeUTF8( id.replaceAll( "/", ":" ) );
+        if ( prop.endsWith( "/" ) ) {
+          prop = prop + id;
+        } else {
+          prop = prop + "/" + id;
+        }
+        if ( !prop.endsWith( "/" ) ) {
+          prop = prop + "/viewer?";
+        } else {
+          prop = prop + "viewer?";
+        }
+      }
+      property = prop;
+    }
+
+    return new TypeValuePair( AnyType.TYPE, property );
+  }
+
+  private static String convertToString( Object o ) {
+    String result = null;
+    if ( o != null && o != NULL_OBJECT ) {
+      result = o.toString();
+    }
+    return result;
   }
 }
