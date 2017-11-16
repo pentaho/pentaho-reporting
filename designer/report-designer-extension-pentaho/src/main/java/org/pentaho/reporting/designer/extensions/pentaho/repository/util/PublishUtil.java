0135cd805267cd90cd4a862e73b0716f7822ebb4
Dmitriy_Stepanov <stepanovdg@gmail.com>
2017-11-16 14:15:04 +0300

    [SP-3851][PRD-5925] In PRD, opening a file containing surrogate pair characters in the file name from the repository results in an error.

--- designer/report-designer-extension-pentaho/src/main/java/org/pentaho/reporting/designer/extensions/pentaho/repository/util/PublishUtil.java
+++ designer/report-designer-extension-pentaho/src/main/java/org/pentaho/reporting/designer/extensions/pentaho/repository/util/PublishUtil.java
@@ -37,7 +37,7 @@ import org.pentaho.reporting.engine.classic.core.modules.parser.bundle.writer.Bu
 import org.pentaho.reporting.libraries.base.config.Configuration;
 import org.pentaho.reporting.libraries.base.util.IOUtils;
 import org.pentaho.reporting.libraries.base.util.StringUtils;
-import java.net.URLEncoder;
+import org.pentaho.reporting.libraries.base.util.URLEncoder;
 import org.pentaho.reporting.libraries.pensol.PentahoSolutionsFileSystemConfigBuilder;
 import org.pentaho.reporting.libraries.pensol.PublishRestUtil;
 import org.pentaho.reporting.libraries.repository.ContentIOException;
