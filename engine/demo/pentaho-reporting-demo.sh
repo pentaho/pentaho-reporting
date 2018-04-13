b95a6cb0218ae3426079e88bf2a9e6692d426b4a
Vitor Cardoso <vitor.cardoso@pentaho.com>
2018-04-13 14:49:31 +0100

    [BACKLOG-22796] - 8.1 - Update Copyright Script have 2017 instead of 2018

--- engine/demo/pentaho-reporting-demo.sh
+++ engine/demo/pentaho-reporting-demo.sh
@@ -14,7 +14,7 @@
 # without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 # See the GNU Lesser General Public License for more details.
 #
-# Copyright (c) 2013-2017 Hitachi Vantara..  All rights reserved.
+# Copyright (c) 2013-2018 Hitachi Vantara..  All rights reserved.
 # -----------------------------------------------------------------------------------------------
 
 java -XX:MaxPermSize=512m -jar launcher.jar
