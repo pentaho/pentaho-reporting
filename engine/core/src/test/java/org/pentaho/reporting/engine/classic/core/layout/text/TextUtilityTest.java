547dac2c6fcfe10a813329ee153b6f14ac2b5029
Pedro Patricio <pedro.patricio@hitachivantara.com>
2019-02-25 15:59:54 +0000

    [SP-4949] Backport of PRD-6049 - Report Sub Title 1 is not being exported in Excel [Excel + Excel (2007)] (7.1 Suite)

--- engine/core/src/test/java/org/pentaho/reporting/engine/classic/core/layout/text/TextUtilityTest.java
+++ engine/core/src/test/java/org/pentaho/reporting/engine/classic/core/layout/text/TextUtilityTest.java
@@ -12,7 +12,7 @@
  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU Lesser General Public License for more details.
  *
- * Copyright (c) 2001 - 2018 Object Refinery Ltd, Hitachi Vantara and Contributors..  All rights reserved.
+ * Copyright (c) 2001 - 2019 Object Refinery Ltd, Hitachi Vantara and Contributors..  All rights reserved.
  */
 
 package org.pentaho.reporting.engine.classic.core.layout.text;
@@ -63,6 +63,42 @@ public class TextUtilityTest {
     when( fontMetrics.getMaxHeight() ).thenReturn( maxHeight );
     when( fontMetrics.getMaxAscent() ).thenReturn( (long) 7500 );
     when( fontMetrics.getMaxDescent() ).thenReturn( (long) 7500 );
+    BaselineInfo baseLineInfo = new BaselineInfo();
+    baseLineInfo.setDominantBaseline(BaselineInfo.MIDDLE);
+    when( fontMetrics.getBaselines( anyInt(), any(BaselineInfo.class))).thenReturn( baseLineInfo );
+
+    DefaultExtendedBaselineInfo baseLine = (DefaultExtendedBaselineInfo)TextUtility.createPaddedBaselineInfo( 'x', fontMetrics, null );
+    Assert.assertTrue( baseLine.getBaselines()[ExtendedBaselineInfo.AFTER_EDGE] >= RenderableText.convert(maxHeight) );
+  }
+
+  @Test
+  public void testCreateCorrectBaselineWithMaxHeightGreaterThanAscentPlusDescent() throws Exception {
+    PowerMockito.mockStatic( TextUtility.class );
+    when(TextUtility.createPaddedBaselineInfo( anyInt(), any(FontMetrics.class), any(BaselineInfo.class) )).thenCallRealMethod();
+
+    long maxHeight = (long) 15000;
+    FontMetrics fontMetrics = mock( FontMetrics.class );
+    when( fontMetrics.getMaxHeight() ).thenReturn( maxHeight );
+    when( fontMetrics.getMaxAscent() ).thenReturn( (long) 4500 );
+    when( fontMetrics.getMaxDescent() ).thenReturn( (long) 4500 );
+    BaselineInfo baseLineInfo = new BaselineInfo();
+    baseLineInfo.setDominantBaseline(BaselineInfo.MIDDLE);
+    when( fontMetrics.getBaselines( anyInt(), any(BaselineInfo.class))).thenReturn( baseLineInfo );
+
+    DefaultExtendedBaselineInfo baseLine = (DefaultExtendedBaselineInfo)TextUtility.createPaddedBaselineInfo( 'x', fontMetrics, null );
+    Assert.assertEquals( baseLine.getBaselines()[ExtendedBaselineInfo.AFTER_EDGE], RenderableText.convert(maxHeight) );
+  }
+
+  @Test
+  public void testCreateCorrectBaselineWithMaxHeightLowerThanAscentPlusDescent() throws Exception {
+    PowerMockito.mockStatic( TextUtility.class );
+    when(TextUtility.createPaddedBaselineInfo( anyInt(), any(FontMetrics.class), any(BaselineInfo.class) )).thenCallRealMethod();
+
+    long maxHeight = (long) 15000;
+    FontMetrics fontMetrics = mock( FontMetrics.class );
+    when( fontMetrics.getMaxHeight() ).thenReturn( maxHeight );
+    when( fontMetrics.getMaxAscent() ).thenReturn( (long) 15000 );
+    when( fontMetrics.getMaxDescent() ).thenReturn( (long) 15000 );
     BaselineInfo baseLineInfo = new BaselineInfo();
     baseLineInfo.setDominantBaseline(BaselineInfo.MIDDLE);
     when( fontMetrics.getBaselines( anyInt(), any(BaselineInfo.class))).thenReturn( baseLineInfo );
