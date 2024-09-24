e216a161e0f2e1575ded3b168372d477b7bb9471
lgrill-pentaho <larry.grill@hitachivantara.com>
2024-09-24 18:10:28 -0400

    Revert "HNC-766: updated license and copyright headers"
    This reverts commit 926e6e8a6df40b6c00d8c609f5899ab64eeecb22.

--- engine/legacy-charts/src/main/java/org/pentaho/plugin/jfreereport/reportcharts/ExtendedXYLineChartExpression.java
+++ engine/legacy-charts/src/main/java/org/pentaho/plugin/jfreereport/reportcharts/ExtendedXYLineChartExpression.java
@@ -1,14 +1,15 @@
-/*! ******************************************************************************
- *
- * Pentaho
- *
- * Copyright (C) 2024 by Hitachi Vantara, LLC : http://www.pentaho.com
- *
- * Use of this software is governed by the Business Source License included
- * in the LICENSE.TXT file.
- *
- * Change Date: 2028-08-13
- ******************************************************************************/
+/*
+* Copyright 2002 - 2017 Hitachi Vantara.  All rights reserved.
+* 
+* This software was developed by Hitachi Vantara and is provided under the terms
+* of the Mozilla Public License, Version 1.1, or any later version. You may not use
+* this file except in compliance with the license. If you need a copy of the license,
+* please go to http://www.mozilla.org/MPL/MPL-1.1.txt. TThe Initial Developer is Pentaho Corporation.
+*
+* Software distributed under the Mozilla Public License is distributed on an "AS IS"
+* basis, WITHOUT WARRANTY OF ANY KIND, either express or  implied. Please refer to
+* the license for the specific language governing your rights and limitations.
+*/
 
 package org.pentaho.plugin.jfreereport.reportcharts;
 
