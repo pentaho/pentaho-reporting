ef1cd2ba9b872af101d83db010b1ac24882c0bb8
lgrill-pentaho <larry.grill@hitachivantara.com>
2024-11-11 13:20:09 -0500

    DEVO-11194: updated license and copyright headers

--- engine/core/src/main/java/org/pentaho/reporting/engine/classic/core/modules/misc/connections/parser/DatabaseConnectionsXmlFactoryModule.java
+++ engine/core/src/main/java/org/pentaho/reporting/engine/classic/core/modules/misc/connections/parser/DatabaseConnectionsXmlFactoryModule.java
@@ -1,3 +1,14 @@
+/*! ******************************************************************************
+ *
+ * Pentaho
+ *
+ * Copyright (C) 2024 by Hitachi Vantara, LLC : http://www.pentaho.com
+ *
+ * Use of this software is governed by the Business Source License included
+ * in the LICENSE.TXT file.
+ *
+ * Change Date: 2028-08-13
+ ******************************************************************************/
 package org.pentaho.reporting.engine.classic.core.modules.misc.connections.parser;
 
 import org.pentaho.reporting.engine.classic.core.modules.misc.connections.ConnectionModule;
