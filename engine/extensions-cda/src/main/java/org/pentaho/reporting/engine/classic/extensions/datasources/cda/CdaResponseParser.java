ce012b48d91b5bf21fe9d5b501f78ef5bbea7803
joana-fb <132680682+joana-fb@users.noreply.github.com>
2025-06-12 16:50:34 +0100

    fix: remove unused imports [PPP-5719]

--- engine/extensions-cda/src/main/java/org/pentaho/reporting/engine/classic/extensions/datasources/cda/CdaResponseParser.java
+++ engine/extensions-cda/src/main/java/org/pentaho/reporting/engine/classic/extensions/datasources/cda/CdaResponseParser.java
@@ -25,7 +25,6 @@ import org.xml.sax.SAXException;
 import org.xml.sax.XMLReader;
 import org.xml.sax.helpers.DefaultHandler;
 
-import javax.xml.XMLConstants;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
--- libraries/libpensol/src/main/java/org/pentaho/reporting/libraries/pensol/vfs/XmlSolutionFileModel.java
+++ libraries/libpensol/src/main/java/org/pentaho/reporting/libraries/pensol/vfs/XmlSolutionFileModel.java
@@ -24,7 +24,6 @@ import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 import org.xml.sax.XMLReader;
 
-import javax.xml.XMLConstants;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
